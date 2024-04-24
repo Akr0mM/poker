d6e62f737d411912e649b0b788917d8377cda442
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2024-04-24 22:23:43 +0200

    create and join table

--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -1,3 +1,5 @@
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
 import './home.html';
 import './home.css';
 
@@ -7,6 +9,62 @@ Template.home.onCreated(function () {
 });
 
 Template.home.events({
+  'click .submit-join'() {
+    const name = $('#name-input').val();
+
+    if (name !== '') {
+      Meteor.call('tables.join', name, error => {
+        if (error) {
+          console.log(error);
+        } else {
+          FlowRouter.go(`/table?name=${name}`);
+        }
+      });
+    } else {
+      console.warn('Name is required');
+    }
+  },
+
+  'click .submit-create'() {
+    const name = $('#name-input').val();
+    const buyIn = parseFloat($('#buy-in-input').val(), 10);
+    const bb = parseFloat($('#bb').text(), 10);
+    const sb = parseFloat($('#sb').text(), 10);
+
+    if (name !== '' && !Number.isNaN(buyIn)) {
+      Meteor.call('tables.create', name, buyIn, bb, sb, error => {
+        if (error) {
+          console.log(error);
+        } else {
+          Meteor.call('tables.join', name, err => {
+            if (err) {
+              console.log(err);
+            } else {
+              FlowRouter.go(`/table?name=${name}`);
+            }
+          });
+        }
+      });
+    } else {
+      console.warn('Name and buy-in are required');
+    }
+  },
+
+  'input #buy-in-input'(event) {
+    const { value } = event.target;
+    const bb = $('#bb');
+    const sb = $('#sb');
+
+    if (value === '') {
+      bb.text('~');
+      sb.text('~');
+    } else {
+      const smb = value / 200;
+      bb.text(smb * 2);
+      sb.text(smb);
+    }
+  },
+
   'click .create'() {
     Template.instance().create.set(true);
   },
